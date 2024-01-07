13ae207344ec0190456f249dd0006d5acd70e547
fazilshafibhat <fazilshafi.fsb382@gmail.com>
2024-01-07 11:03:13 +0530

    project setup

---
+++ src/test/java/com/self_learning/q_money/QMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self_learning.q_money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
